b3286a0553dd72428ae68115ff033092222459e4
Tu Nombre <darwingabriel909@gmail.com>
2026-05-01 10:01:21 -0400

    Aprendiendo a usar componentes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import logo from './platzi.webp';
-import './App.css';
+import logo from "./platzi.webp";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <TodoItiem />
+      <TodoItiem />
+      <TodoItiem />
+      <TodoCount completed={2} total={3} />
+      <TodoCount completed={8} total={10} />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -15,10 +20,28 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprende React subnormal
         </a>
       </header>
     </div>
+  );
+}
+
+function TodoItiem() {
+  return (
+    <li>
+      <br />
+      <span>V</span>
+      <p>Llorar con la Llorona</p>
+      <span>X</span>
+    </li>
+  );
+}
+function TodoCount(props) {
+  return (
+    <p>
+      {props.completed} de {props.total} tareas completadas
+    </p>
   );
 }
 
